5ceedfb624f3a6bdd3ffcecd55d876859b1e15df
Adam Sharp <adam@sharplet.me>
2018-02-09 17:22:33 -0500

    Add ability to specify .env file to load
    Uses clap for argument parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+extern crate clap;
 extern crate dotenv;
 extern crate exec;
+
+use clap::App;
+use clap::AppSettings;
+use clap::Arg;
+use exec::Command;
 
 macro_rules! die {
     ($msg:expr) => ({
@@ -9,14 +15,36 @@ macro_rules! die {
 }
 
 fn main() {
-    dotenv::dotenv().ok();
+    let matches = App::new("dotenv")
+        .about("Run a command using the environment in a .env file")
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .setting(AppSettings::TrailingVarArg)
+        .arg(Arg::with_name("file")
+             .short("f")
+             .long("file")
+             .takes_value(true)
+             .help("Use a specific .env file (defaults to .env)"))
+        .arg(Arg::with_name("command")
+             .required(true))
+        .arg(Arg::with_name("args..."))
+        .get_matches();
 
-    let mut args = std::env::args_os().skip(1);
-    let name = args.next().unwrap_or_else(|| die!("no arguments"));
-    let mut command = exec::Command::new(name);
-    for argument in args {
-        command.arg(argument);
-    }
+    match matches.value_of("file") {
+        None => dotenv::dotenv(),
+        Some(file) => dotenv::from_filename(file),
+    }.ok();
+
+    let mut command = Command::new(matches.value_of("command").unwrap());
+
+    match matches.values_of("args") {
+        Some(command_args) => {
+            for argument in command_args {
+                command.arg(argument);
+            }
+        },
+        None => {},
+    };
+
     let error = command.exec();
     die!(error);
 }
